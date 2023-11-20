3d95d6951dc6348401e304fe0a9c4909efffeef5
A. Matías Quezada <amatiasq@gmail.com>
2023-11-20 23:14:04 +0100

    Used Houdini for backgrounds

--- amatiasq.com-v2/public/dot-mesh.js
+++ amatiasq.com-v2/public/dot-mesh.js
@@ -1,70 +1,228 @@
-const numberProps = [
-  '--t',
-  '--points',
-  '--points-x',
-  '--points-y',
-]
+function log(x) {
+  console.log(x);
+  return x;
+}
 
-const allProps = [
-  ...numberProps,
-  '--color-background',
-  '--color-text',
-];
-
-const rand = (max, min = 0) => Math.random() * (max - min) + min;
-const VELOCITY = 0.1;
-
-registerPaint('dot-mesh', class {
+class GridWorklet {
   static get inputProperties() {
-    return [...allProps];
-  }
-
-  #parseProps(props) {
-    return Object.fromEntries(
-      allProps.map(propName => [propName, parse(propName)])
-    );
-
-    function parse(name) {
-      const val = props.get(name).toString().trim();
-      if (!numberProps.includes(name)) return val;
-      const result = parseFloat(val);
-      if (!isNaN(result)) return result;
-    }
-  }
-
-  paint(ctx, { width: w, height: h }, props) { //, [{ value: x }, { value: y }]) {
-    const {
-      '--t': t,
-      '--color-background': bg = 'white',
-      '--color-text': fg = 'black',
-      '--points': points = 10,
-    } = this.#parseProps(props);
-
-    this.points ??= Array.from({ length: points }, () => ({
-      x: rand(w),
-      y: rand(h),
-      vx: rand(VELOCITY, -VELOCITY),
-      vy: rand(VELOCITY, -VELOCITY),
+    return log(workletProperties({
+      color: [
+        '--color-background',
+        '--color-text',
+        '--grid-color',
+      ],
+      numeric: [
+        '--grid-size',
+        // '--grid-columns',
+        // '--grid-width',
+        // '--grid-height',
+      ],
     }));
+  }
+
+  paint(ctx, { width: w, height: h }, props) {
+    const {
+      '--color-background': bg = 'black',
+      '--color-text': fg = 'grey',
+      '--grid-color': cg = fg,
+      '--grid-columns': gc,
+      '--grid-size': gs = 10,
+      '--grid-width': gw = gc ? w / gc : gs,
+      '--grid-height': gh = gc ? gw : gs,
+    } = GridWorklet.inputProperties.parse(props);
+
+    if (gs < 1 || gw < 1 || gh < 1) return;
 
     ctx.fillStyle = bg;
     ctx.fillRect(0, 0, w, h);
-    ctx.fillStyle = fg;
+    ctx.strokeStyle = cg;
 
-    const circle = (x, y, radius = 2) => ctx.arc(x, y, radius, 0, 2 * Math.PI);
+    const drawer = this.init(props);
 
-    // console.log(t)
+    for (let i = 0; i <= w; i += gw) {
+      for (let j = 0; j <= h; j += gh) {
+        drawer(ctx, i, j, i + gw, j + gh);
+      }
+    }
+  }
 
-    for (const point of this.points) {
-      const x = (point.x + point.vx * t) % w;
-      const y = (point.y + point.vy * t) % h;
+  init(props) {
+    return this.drawCell;
+  }
 
+  drawCell(ctx, x, y, x2, y2) {
+    throw new Error('Not implemented');
+  }
+}
+
+registerPaint('dotted-grid', class DottedGrid extends GridWorklet {
+  static get inputProperties() {
+    return log(GridWorklet.inputProperties.extend({
+      numeric: [
+        '--grid-point-size',
+      ]
+    }));
+  }
+
+  init(props) {
+    const {
+      '--grid-point-size': diameter = 1,
+    } = DottedGrid.inputProperties.parse(props);
+
+    const radius = diameter / 2;
+
+    return (ctx, x, y) => this.drawCell(ctx, x, y, radius);
+  }
+
+  drawCell(ctx, x, y, radius) {
+    if (randBool()) {
       ctx.beginPath();
-      circle(
-        x < 0 ? x + w : x,
-        y < 0 ? y + h : y
-      );
-      ctx.fill();
+      ctx.arc(x, y, radius, 0, 2 * Math.PI)
+      ctx.stroke();
     }
   }
 });
+
+registerPaint('grid', class extends GridWorklet {
+  drawCell(ctx, x, y, x2, y2) {
+    // horizontal
+    if (randBool()) {
+      ctx.beginPath();
+      ctx.moveTo(x, y2);
+      ctx.lineTo(x2, y2);
+      ctx.stroke();
+    }
+
+    // vertical
+    if (randBool()) {
+      ctx.beginPath();
+      ctx.moveTo(x2, y);
+      ctx.lineTo(x2, y2);
+      ctx.stroke();
+    }
+  }
+});
+
+registerPaint('diagonal-grid', class extends GridWorklet {
+  drawCell(ctx, x, y, x2, y2) {
+    if (randBool()) {
+      ctx.beginPath();
+      ctx.moveTo(x, y);
+      ctx.lineTo(x2, y2);
+      ctx.stroke();
+    } else {
+      ctx.beginPath();
+      ctx.moveTo(x2, y);
+      ctx.lineTo(x, y2);
+      ctx.stroke();
+    }
+  }
+});
+
+function randBool() {
+  return Math.random() < 0.5;
+}
+
+function workletProperties({ text = [], numeric = [], color = [] }) {
+  const all = numeric.concat(color).concat(text);
+
+  return Object.assign(all, {
+    parse(input) {
+      return Object.fromEntries(
+        all.map(propName => [propName, parse(propName, input.get(propName))])
+      );
+    },
+
+    extend({ text: childText = [], numeric: childNumeric = [], color: childColor = [] }) {
+      return workletProperties({
+        text: [...text, ...childText],
+        numeric: [...numeric, ...childNumeric],
+        color: [...color, ...childColor],
+      });
+    }
+  });
+
+  function parse(name, value) {
+    console.log(name, value)
+    const val = value?.toString().trim();
+    if (!val) return undefined;
+    if (!numeric.includes(name)) return val;
+    const result = parseFloat(val);
+    if (!isNaN(result)) return result;
+  }
+
+}
+
+// function drawDigit(context, value, x, y, height) {
+//   if (value === 1) {
+//     drawOne(context, x, y, height);
+//   } else if (value === 0) {
+//     drawZero(context, x, y, height);
+//   }
+// }
+
+// function drawOne(context, x, y, height) {
+//   const width = height / 2;
+//   const startX = x + width / 2;
+
+//   context.beginPath();
+//   context.moveTo(startX, y);
+//   context.lineTo(startX, y + height);
+//   context.stroke();
+// }
+
+// function drawZero(context, x, y, height) {
+//   const width = height / 2;
+//   const centerX = x + width / 2;
+//   const centerY = y + height / 2;
+//   const radius = width / 2;
+
+//   context.beginPath();
+//   context.arc(centerX, centerY, radius, 0, 2 * Math.PI);
+//   context.stroke();
+// }
+
+// function drawFancyOne(context, x, y, height) {
+//   const width = height / 2;
+//   const baseX = x + width / 2;
+//   const topY = y + height * 0.2;
+
+//   // Draw the slanting part of '1'
+//   context.beginPath();
+//   context.moveTo(baseX - width * 0.2, topY);
+//   context.lineTo(baseX, y);
+//   context.lineTo(baseX, y + height);
+//   context.stroke();
+
+//   // Draw the base of '1'
+//   context.beginPath();
+//   context.moveTo(baseX - width * 0.2, y + height);
+//   context.lineTo(baseX + width * 0.2, y + height);
+//   context.stroke();
+// }
+
+// function drawFancyZero(context, x, y, height) {
+//   const width = height / 2;
+//   const centerX = x + width / 2;
+//   const centerY = y + height / 2;
+//   const radius = width / 2;
+
+//   // Outer circle
+//   context.beginPath();
+//   context.arc(centerX, centerY, radius, 0, 2 * Math.PI);
+//   context.stroke();
+
+//   // Inner circle for the '0' to give a donut shape
+//   context.beginPath();
+//   context.arc(centerX, centerY, radius * 0.6, 0, 2 * Math.PI);
+//   context.stroke();
+// }
+
+// // Example usage
+// function drawFancyDigit(context, value, x, y, height) {
+//   if (value === 1) {
+//     drawFancyOne(context, x, y, height);
+//   } else if (value === 0) {
+//     drawFancyZero(context, x, y, height);
+//   }
+// }
